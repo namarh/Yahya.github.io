4287aeaae5ad358cfb1a37f5dc6b2884a3ea8067
Yahya alshamrani <89461932+namarh@users.noreply.github.com>
2026-03-12 16:16:32 +0300

    Update test.cpp

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,1 +1,26 @@
 
+#include <iostream>
+#include<string>
+using namespace std;
+int ReadPositiveNumber(string messag) {
+
+	int Number = 0;
+	do
+	{
+		cout << messag << endl;
+		cin >> Number;
+	} while (Number<=0);
+	return Number;
+}
+int Multible(int Number) {
+	return Number * Number;
+}
+int main()
+{
+	int x = ReadPositiveNumber("Enter Positive Number");
+	
+	cout << "Number * Number = "<< Multible(x) << endl;
+	return 0;
+}
+
+
